2bdf96855472a7e810b630ecab26fb04df6e2233
tholivera <emaildathaisoliveira.silva@gmail.com>
2021-11-03 22:04:07 -0300

    Adicionando tema

--- BlogPessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
+++ BlogPessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import br.org.generation.blogpessoal.model.Postagem;
 
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
